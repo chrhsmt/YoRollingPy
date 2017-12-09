9484276e128729ec0702551dedd95570a2e14981
chihiro hashimoto <chr@chrhsmt.com>
2017-12-10 08:16:52 +0900

    Respond if captions is empty

--- cv.py
+++ cv.py
@@ -20,20 +20,19 @@ params = {
 
 def analyze(path):
     print("- analyze -")
-    # Replace the three dots below with the full file path to a JPEG image of a celebrity on your computer or network.
-    image = open(path,'rb').read() # Read image file in binary mode
+    image = open(path,'rb').read()
 
     try:
-        # NOTE: You must use the same location in your REST call as you used to obtain your subscription keys.
-        #   For example, if you obtained your subscription keys from westus, replace "westcentralus" in the 
-        #   URL below with "westus".
         response = requests.post(url = 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze',
                                  headers = headers,
                                  params = params,
                                  data = image)
         data = response.json()
         # print(data["description"]["captions"][0]["text"])
-        return data["description"]["captions"][0]["text"]
+        if len(data["description"]["captions"]) == 0:
+            tags = data["description"]["tags"]
+            return " ".join(tags[0:int(len(tags) / 2)])
+        else:
+            return data["description"]["captions"][0]["text"]
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
-    ####################################
